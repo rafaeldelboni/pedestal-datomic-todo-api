72eca9a1202a3c09357b8ea09239c3dffcec9a9e
Rafael Delboni <rafadelboni@gmail.com>
2019-01-29 23:22:10 -0200

    First commit

---
+++ test/ring_datomic_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-datomic-api.core-test
+  (:require [clojure.test :refer :all]
+            [ring-datomic-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
