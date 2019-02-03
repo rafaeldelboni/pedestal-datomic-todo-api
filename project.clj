d2359d118d011e50bf35032809f9834da544b46c
Rafael Delboni <rafadelboni@gmail.com>
2019-02-03 00:58:32 -0200

    Feat: adds datomic/free deps

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.datomic/datomic-free "0.9.5697"]]
   :main ^:skip-aot ring-datomic-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
