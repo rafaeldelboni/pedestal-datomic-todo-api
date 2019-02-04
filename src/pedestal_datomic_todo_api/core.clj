d9147ad30aff1af2f84e5b3130fcad975654eaea
Rafael Delboni <rafadelboni@gmail.com>
2019-02-04 21:46:12 -0200

    Feat: adds crud for db interaction

--- src/pedestal_datomic_todo_api/core.clj
+++ src/pedestal_datomic_todo_api/core.clj
@@ -32,19 +32,40 @@
     @(d/transact conn schema)
     conn))
 
-(def query-all-todos '[:find ?e ?id ?done ?text
+(def query-todos '[:find ?e ?id ?done ?text
                        :where [?e :todo/id ?id]
                               [?e :todo/done? ?done]
                               [?e :todo/text ?text]])
 
-(defn query-create-todo
-  [tempid text]
-  [{:db/id tempid
-    :todo/id   (d/squuid)
-    :todo/done? false
-    :todo/text text}])
+(defn db-build-todo
+  [tempid id text done?]
+  [{:db/id      tempid
+    :todo/id    id
+    :todo/text  text
+    :todo/done? done?}])
 
-;@(d/transact conn (query-create-todo (d/tempid :db.part/user) "learn clj"))
+(defn db-new-todo
+  [tempid text]
+  (db-build-todo tempid (d/squuid) text false))
+
+(defn db-delete-todo
+  [id]
+  [[:db.fn/retractEntity [:todo/id id]]])
+
+(defn create-todo
+  [conn text]
+  (let [todo (db-new-todo (d/tempid :db.part/user) text)]
+    @(d/transact conn todo)
+    todo))
+
+;create
+;@(d/transact conn (db-new-todo (d/tempid :db.part/user) "learn clj"))
+;read
+;(d/q query-all-todos (d/db conn))
+;update
+;@(d/transact conn (db-build-todo (d/tempid :db.part/user) #uuid "5c58c42a-1764-40bf-9c9b-49683a87d9f0" "changeme dude!!!!" true))
+;delete
+;@(d/transact conn (db-delete-todo #uuid "5c58c42a-1764-40bf-9c9b-49683a87d9f0"))
 
 (defn -main
   "I don't do a whole lot ... yet."
