2e243202f5354275dd592b1865a3eef9d77d17cd
Rafael Delboni <rafadelboni@gmail.com>
2019-02-12 23:49:12 -0200

    Feat: adds database protocols

---
+++ src/pedestal_datomic_todo_api/components/storage_datomic.clj
@@ -0,0 +1,52 @@
+(ns pedestal-datomic-todo-api.components.storage-datomic
+  (:require [pedestal-datomic-todo-api.protocols.storage-client :as storage-client]
+            [com.stuartsierra.component :as component]
+            [datomic.api :as d]))
+
+(def ^:private schema
+  [{:db/doc         "todo unique id"
+    :db/ident       :todo/id
+    :db/index       true
+    :db/valueType   :db.type/uuid
+    :db/unique      :db.unique/identity
+    :db/cardinality :db.cardinality/one
+    :db/id          (d/tempid :db.part/db)}
+   {:db/doc         "todo done flag"
+    :db/ident       :todo/done?
+    :db/index       true
+    :db/valueType   :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/id          (d/tempid :db.part/db)}
+   {:db/doc         "todo description"
+    :db/ident       :todo/text
+    :db/index       true
+    :db/fulltext    true
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/id          (d/tempid :db.part/db)}])
+
+(defn init-connect-to-database [db-uri]
+  (d/create-database db-uri)
+  (let [conn (d/connect db-uri)]
+    @(d/transact conn schema)
+    conn))
+
+(defrecord StorageDatomic [uri connection]
+  component/Lifecycle
+
+  (start [component]
+    (println ";; Starting database")
+    (let [conn (init-connect-to-database uri)]
+      (assoc component :connection conn)))
+
+  (stop [component]
+    (println ";; Stopping database")
+    (.close connection)
+    (assoc component :connection nil))
+
+  storage-client/StorageClient
+  (query [_this data args] (d/q data connection args))
+  (exec! [_this data] @(d/transact connection data)))
+
+(defn new-storage-datomic [uri]
+  (map->StorageDatomic {:uri uri}))
---
+++ src/pedestal_datomic_todo_api/protocols/storage_client.clj
@@ -0,0 +1,7 @@
+(ns pedestal-datomic-todo-api.protocols.storage-client)
+
+(defprotocol StorageClient
+  "Protocol for simple storage mechanism; simple but not practical in any way"
+  (query [conn data] "Return the contents of storage based on datalog")
+  (exec! [conn data] "Mutate the storage with the provided function"))
+
